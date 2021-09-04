6d6446985e475a81dcbd6bafb4dc6f5030f9eb0b
RaZegame <75721601+RaZegame@users.noreply.github.com>
2021-09-04 18:03:23 +0300

    some stuff for publish

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rpc
+Use `npm start` to start, build the repo with `npm run build`
--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import rpc from "discord-rpc";
-
+import dotenv from "dotenv";
 const client = new rpc.Client({ transport: "ipc" });
 
-client.on("ready", async():Promise<void> => {
-  
+client.on('ready', async():Promise<void> => {
    await client.setActivity({
         details: "Coding nerd, loves sloths",
         state: "My bot, if you interested in finding new people!",
@@ -12,7 +11,7 @@ client.on("ready", async():Promise<void> => {
         smallImageKey: "blurple_sloth",
         buttons: [{ label: "Slothy invite", url: "https://discord.com/oauth2/authorize?client_id=826056261610373130&permissions=3761564736&scope=bot%20applications.commands" }, { label: "Epic server", url: "https://discord.com/invite/KDvhzHFfBj" }]
     });
-  console.log('Authed for user', client.user.username);
+    console.log('Presence made for', client.user.username);
 });
-
-client.login({ clientId: "822768554042589234" });
+dotenv.config();
+client.login({ clientId: process.env.clientId });
